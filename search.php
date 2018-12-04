e7a4eee3ecded2629841451e654526de4b2aaed0
VirtusDesktopPC <vbo@rastec.dk>
2018-12-04 20:56:48 +0100

    mere

--- search.php
+++ search.php
@@ -1,3 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Voogle - Google Clone</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <div class="header-content">
 
-$term = $_GET['term'];
+            <div class="logo-container">
+                <a href="index.php">
+                    <img src="assets/images/logo.png" alt="Voogle logo">
+                </a>
+            </div>
+
+            <div class="search-container">
+                <form action="search.php" method="GET">
+                    <div class="search-bar-container">
+                        <input type="text" class="search-box" name="term">
+                        <button>Search</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>

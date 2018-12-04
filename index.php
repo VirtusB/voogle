3359994cf207765c8a34842ebeacd50e3ca0f55e
VirtusDesktopPC <vbo@rastec.dk>
2018-12-04 20:23:39 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Voogle - Google Clone</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<div class="wrapper index-page">
+    <div class="main-section">
+        <div class="logo-container">
+            <img src="assets/images/logo.png" alt="Voogle logo">
+        </div>
+        <div class="search-container">
+            <form action="search.php" method="GET">
+                <input autocomplete="off" name="term" class="search-box" type="text">
+                <input value="Search" class="search-button" type="submit">
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,3 @@
+<?php
+
+$term = $_GET['term'];
